7ab985343cb542816ba784e5dbc859c1668ca756
Zack White <zrwhite2993@gmail.com>
2020-01-08 14:32:54 -0600

    Project ready for deployment. Fixed errors.(Confusion between function types)

--- 02-Homework/Develop/server.js
+++ 02-Homework/Develop/server.js
@@ -1,51 +1,23 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const mongojs = require('mongojs');
-const path = require('path');
 
 const PORT = process.env.PORT || 3000;
-
-const User = require("./models/workout.js");
 const app = express();
-
-app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-const databaseUrl = 'workoutTracker';
+/* const databaseUrl = 'Workout';
 const collections = ['workout'];
 
-const db = mongojs(databaseUrl, collections);
+const db = require('./models') */
 
-db.on('error', error => {
-  console.log('Database Error:', error);
-});
+require("./routes/apiRoutes.js")(app);
+require("./routes/htmlRoutes.js")(app);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/index.html"))
-})
-
-app.get("/exercise", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/exercise.html"))
-});
-
-app.get("/api/workouts", (req, res) => {
-  console.log(req.body);
-
-  db.workout.insert(req.body, (error, data) => {
-    if (error) {
-      res.send(error);
-    } else {
-      res.send(data);
-    }
-  });
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
